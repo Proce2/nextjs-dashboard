3f27196e56a06abb15ca3e982c132848d817504e
Proce2 <david.jmoraes@outlook.com>
2023-12-01 22:26:43 -0300

    Second commit

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,8 +1,8 @@
 import { Inter, Lusitana } from 'next/font/google';
 
-// define your variable fonts
-const inter = Inter({ subsets: ['latin'] });
-const lusitana = Lusitana({ weight: '400', subsets: ['latin'] });
-
-// use named exports
-export { inter, lusitana };
+export const inter = Inter({ subsets: ['latin'] });
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
